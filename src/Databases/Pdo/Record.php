4bb43b9f0416ca9965f98a18a6e91aa6300d66c5
Patrick Morpey <patrick@morphp.nl>
2022-03-24 11:09:23 +0100

    StoreRecords should implement ArrayAccess

--- src/Databases/Pdo/Record.php
+++ src/Databases/Pdo/Record.php
@@ -30,4 +30,24 @@ class Record implements StoreRecord
     {
         $this->data = array_merge($this->data, $values);
     }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return array_key_exists($offset, $this->data);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->data[$offset];
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->data[$offset] = $value;
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->data[$offset]);
+    }
 }
--- src/Interfaces/StoreRecord.php
+++ src/Interfaces/StoreRecord.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\StorageManager\Interfaces;
 
-interface StoreRecord
+interface StoreRecord extends \ArrayAccess
 {
     public function data(): array;
 
