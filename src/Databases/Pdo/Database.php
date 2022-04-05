5f12b02de8a6d58d8d94870ad6016832d574d8a4
Patrick Morpey <patrick@morphp.nl>
2022-04-05 15:26:23 +0200

    Use persistent connections

--- src/Databases/Pdo/Database.php
+++ src/Databases/Pdo/Database.php
@@ -51,6 +51,7 @@ class Database implements Storage
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES => false,
+            \PDO::ATTR_PERSISTENT => true,
         ];
 
         $this->pdo = new \PDO($this->dns, $this->username, $this->password, $options);
