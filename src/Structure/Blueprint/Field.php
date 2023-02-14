4a793b8d7e1602957647a68f070d00acaf94839c
Patrick Morpey <patrick@morphp.nl>
2023-02-14 17:35:10 +0100

    maxValue might be a float, if the value is larger than PHP_INT_MAX

--- src/Structure/Blueprint/Field.php
+++ src/Structure/Blueprint/Field.php
@@ -9,18 +9,18 @@ use Medas\EntityManager\Types\Integer;
 class Field
 {
     public function __construct(
-        public string $name,
-        public Type   $type,
-        public bool   $isNullable = false,
-        public bool   $isGenerated = false,
-        public bool   $isCreationTimestamp = false,
-        public bool   $isModificationTimestamp = false,
-        public bool   $hasDefault = false,
-        public mixed  $default = null,
-        public int    $minValue = 0,
-        public int    $maxValue = Integer::UNSIGNED_4_BYTE_MAX,
-        public int    $minLength = 0,
-        public int    $maxLength = Integer::UNSIGNED_1_BYTE_MAX,
+        public string    $name,
+        public Type      $type,
+        public bool      $isNullable = false,
+        public bool      $isGenerated = false,
+        public bool      $isCreationTimestamp = false,
+        public bool      $isModificationTimestamp = false,
+        public bool      $hasDefault = false,
+        public mixed     $default = null,
+        public int       $minValue = 0,
+        public int|float $maxValue = Integer::UNSIGNED_4_BYTE_MAX,
+        public int       $minLength = 0,
+        public int       $maxLength = Integer::UNSIGNED_1_BYTE_MAX,
     )
     {
     }
