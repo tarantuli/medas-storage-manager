b508a2c4a5eba0cbba42a1d9eafd7ef2d57b22eb
Patrick Morpey <patrick@morphp.nl>
2023-06-20 15:25:00 +0200

    Return ActionCollections

--- src/UnitOfWork/ActionCollection.php
+++ src/UnitOfWork/ActionCollection.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace Medas\StorageManager\UnitOfWork;
 
 use Medas\Core\Interfaces\Collection;
+use Medas\StorageManager\Interfaces\RecordSet;
 
 interface ActionCollection extends Collection
 {
     public function current(): Action;
+
+    public function execute(): void;
+
+    public function recordSet(): RecordSet;
 }
