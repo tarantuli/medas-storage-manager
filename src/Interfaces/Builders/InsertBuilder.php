7f834dc1cd2e8f27b6db8ac1987112be834f7834
Patrick Morpey <patrick@morphp.nl>
2023-08-22 16:17:19 +0200

    Added priority argument to InsertBuilder->build()

--- src/Interfaces/Builders/InsertBuilder.php
+++ src/Interfaces/Builders/InsertBuilder.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Medas\StorageManager\Interfaces\Builders;
 
 use Medas\StorageManager\Interfaces\Store;
-use Medas\StorageManager\UnitOfWork\ActionSet;
+use Medas\StorageManager\UnitOfWork\{ActionSet, Priority};
 
 interface InsertBuilder
 {
-    public function build(Store $store, array $values): ActionSet;
+    public function build(Store $store, array $values, Priority $priority = Priority::CreateRecord): ActionSet;
 }
