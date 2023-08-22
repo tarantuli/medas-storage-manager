cc71464945a851cd591deefccb075503b301e2d5
Patrick Morpey <patrick@morphp.nl>
2023-08-22 16:05:59 +0200

    Added priority argument to DeleteBuilder->build()

--- src/Interfaces/Builders/DeleteBuilder.php
+++ src/Interfaces/Builders/DeleteBuilder.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Medas\StorageManager\Interfaces\Builders;
 
 use Medas\StorageManager\Interfaces\Store;
-use Medas\StorageManager\UnitOfWork\ActionSet;
+use Medas\StorageManager\UnitOfWork\{ActionSet, Priority};
 
 interface DeleteBuilder
 {
-    public function build(Store $store, array $conditions): ActionSet;
+    public function build(Store $store, array $conditions, Priority $priority = Priority::DeleteRecord): ActionSet;
 }
