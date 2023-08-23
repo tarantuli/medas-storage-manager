4b3ea8522139a891bee16678905df5dfa151bd81
Patrick Morpey <patrick@morphp.nl>
2023-08-23 09:58:35 +0200

    Added executeSet()

--- src/Interfaces/ActionExecutor.php
+++ src/Interfaces/ActionExecutor.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Medas\StorageManager\Interfaces;
 
 use Medas\StorageManager\UnitOfWork\Action;
+use Medas\StorageManager\UnitOfWork\ActionSet;
 
 interface ActionExecutor
 {
     public function execute(Action $action): void;
+
+    public function executeSet(ActionSet $actionSet): void;
 }
