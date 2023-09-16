3927682934201e3fc54eb27443e9e3b3d18212b9
Patrick Morpey <patrick@morphp.nl>
2023-09-16 11:39:53 +0200

    Updated ActionSet

--- src/UnitOfWork/ActionSet.php
+++ src/UnitOfWork/ActionSet.php
@@ -10,6 +10,11 @@ use Medas\StorageManager\Interfaces\RecordSet;
 /** @extends GenericCollection<Action> */
 class ActionSet extends GenericCollection
 {
+    public static function fromAction(Action $action): static
+    {
+        return new static([$action]);
+    }
+
     public RecordSet|null $lastRecordSet = null;
     public mixed $lastInsertId = null;
 }
